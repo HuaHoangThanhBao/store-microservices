03773882b38fcb8e88cfc450c6370781170858c4
Bao <baohua01234@gmail.com>
2023-02-14 17:19:42 +0700

    Add backend microservices

--- packages/core/src/app.module.ts
+++ packages/core/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule.register([
+    {
+      name: 'DETAIL_COMMUNICATION',
+      transport: Transport.TCP
+    },
+    {
+      name: 'PAYMENT_COMMUNICATION',
+      transport: Transport.TCP,
+      options: { port : 3334 }
+    }
+  ])],
   controllers: [AppController],
   providers: [AppService],
 })
--- packages/core/src/app.service.ts
+++ packages/core/src/app.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateUserRequest } from './create-user-request.dto';
+import { CreateUserEvent } from './create-user.event';
 
 @Injectable()
 export class AppService {
+  private readonly users: CreateUserRequest[] = []
+
+  constructor(
+    @Inject('DETAIL_COMMUNICATION') private readonly detailClient: ClientProxy,
+    @Inject('PAYMENT_COMMUNICATION') private readonly paymentClient: ClientProxy
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createUser(createUserRequest: CreateUserRequest) {
+    this.users.push(createUserRequest)
+    this.detailClient.emit('user_created', new CreateUserEvent(createUserRequest.email))
+    this.paymentClient.emit('user_created', new CreateUserEvent(createUserRequest.email))
+  }
+
+  getAnalytics() {
+    return this.paymentClient.send({ cmd: 'get_analytics' }, {});
   }
 }
